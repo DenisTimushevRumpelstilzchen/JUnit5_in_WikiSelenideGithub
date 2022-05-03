a4f62f2c7c09a2f959685c8241877bc0bb5632a2
Your NameDenis <tdv14011984@gmail.com>
2022-05-03 19:10:38 +0300

    add SearchJUnit5InSoftAssertions

--- src/test/java/denis/timushev/SearchJUnit5InSoftAssertions.java
+++ src/test/java/denis/timushev/SearchJUnit5InSoftAssertions.java
@@ -3,9 +3,8 @@ package denis.timushev;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SearchJUnit5InSoftAssertions {
 
@@ -13,8 +12,8 @@ public class SearchJUnit5InSoftAssertions {
     void searchJUnit5Code() {
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
-        $(".markdown-body").should(text("Soft assertions"));
-        $(".markdown-body").$(withText("Soft assertions")).click();
+        $("[id=wiki-pages-filter").setValue("SoftAssertions");
+        $(".wiki-rightbar").$(byText("SoftAssertions")).click();
         $(".markdown-body").shouldBe(text("<div id='first' style='display:none;'>First</div>"));
         $(".markdown-body").shouldBe(text("$(\"#first\").should(visible).click();"));
     }
